3623cd7968d543cddbd82a0f23a242f93ce7645d
rashidsarkar <rashidrock558@gmail.com>
2023-11-17 20:23:42 +0600

    Added useMenu hook and an empty div. (generated by blackbox.ai )

--- src/pages/DashBord/ManageItem/ManageItem.jsx
+++ src/pages/DashBord/ManageItem/ManageItem.jsx
@@ -1,12 +1,15 @@
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import useMenu from "../../../hooks/useMenu";
 
 function ManageItem() {
+  const [menu] = useMenu();
   return (
     <div>
       <SectionTitle
         heading={`msnnsge all items`}
         subHeading={`hurry up`}
       ></SectionTitle>
+      <div></div>
     </div>
   );
 }
